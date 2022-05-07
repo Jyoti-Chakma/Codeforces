9f1d10cf9018125ba6197a016a74ff32a7547111
Jyoti Chakma <jyotichakma.official@outlook.com>
2022-05-07 19:34:03 +0600

    10

---
+++ 281A-Word-Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <cstdio>
+#include <string>
+#include <string.h>
+#include <cmath>
+#include<cstring>
+#include<algorithm>
+ 
+using namespace std;
+ 
+int main()
+{
+    string x;
+    cin >> x;
+    x[0] = toupper(x[0]);
+    cout << x;
+}
